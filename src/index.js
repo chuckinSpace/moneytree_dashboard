b9f54e479f32cbcac24feb76e958a9e573169b1e
Carlos Moyano Rugiero <carlos@Carloss-MacBook-Pro.local>
2020-03-04 19:15:42 -0500

    iniital template react-redux-firebase

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import { render } from "react-dom"
+import { Provider } from "react-redux"
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
+import "firebase/analytics"
+import { createStore, combineReducers, applyMiddleware } from "redux"
+import {
+	ReactReduxFirebaseProvider,
+	firebaseReducer
+} from "react-redux-firebase"
+import { createFirestoreInstance, firestoreReducer } from "redux-firestore"
+import firebaseConfig from "./firebase/firebaseconfig"
+import logger from "redux-logger"
+import App from "./App"
+// react-redux-firebase config
+const rrfConfig = {
+	userProfile: "users",
+	useFirestoreForProfile: true
+}
+console.log(firebaseConfig)
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig)
+// Initialize other services on firebase instance
+firebase.firestore()
+firebase.analytics()
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Add firebase to reducers
+const rootReducer = combineReducers({
+	firebase: firebaseReducer,
+	firestore: firestoreReducer
+})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Create store with reducers and initial state
+const initialState = {}
+const store = createStore(rootReducer, initialState, applyMiddleware(logger))
+
+const rrfProps = {
+	firebase,
+	config: rrfConfig,
+	dispatch: store.dispatch,
+	createFirestoreInstance
+}
+
+render(
+	<Provider store={store}>
+		<ReactReduxFirebaseProvider {...rrfProps}>
+			<App />
+		</ReactReduxFirebaseProvider>
+	</Provider>,
+	document.getElementById("root")
+)
